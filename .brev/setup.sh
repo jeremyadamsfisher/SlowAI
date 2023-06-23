8d22617f738e906d7f6ea9b76a8b3d2f3de92031
jeremy.adams.fisher <jeremy.adams.fisher@gmail.com>
2023-06-23 02:50:21 +0000

    Improve brev setup

---
+++ .brev/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+pip install -q jupyterlab nbdev
+
+if nvidia-smi | grep -q "NVIDIA-SMI has failed because it couldn't communicate with the NVIDIA driver."; then
+  echo "NVIDIA GPU found! Installing GPU-accelerated PyTorch..."
+  pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+else
+  echo "No NVIDIA GPU found! Installing CPU-only PyTorch..."
+  pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+fi
